5348e3f1b5cbda1b17de1afb322ace7579a077e9
yxshas565 <yxshas565@gmail.com>
2026-07-27 11:25:10 +0530

    Updated README.md and some LeetCode approaches

--- dsa/Basics/Lecture_5_Bitwise_Operators.cpp
+++ dsa/Basics/Lecture_5_Bitwise_Operators.cpp
@@ -77,23 +77,80 @@ void prime_or_not(int num){
     }
 }
 
+
+void find_complement(vector <int> num){
+    for (int i=0; i<num.size(); i++){
+        if (num[i] == 0){
+            num[i] = 1;
+        }
+        else if(num[i] == 1){
+            num[i] = 0;
+        }
+    }
+
+    for (int x : num){
+        cout << x << " ";
+    }
+}
+
+
+void decimal_to_binary(int n){
+    vector <int> binary;
+    while (n > 0){
+
+        if (n==0){
+            binary.push_back(0);
+            break;
+        }
+
+        int digit = n % 2;
+        binary.push_back(digit);
+        n/=2;
+    }
+
+    reverse(binary.begin(),binary.end());
+
+    for (int x: binary){
+        cout << x << " ";
+    }
+    cout << endl;
+    find_complement(binary);
+}
+
+void reverse_num(int x){
+    int reverse_num = 0;
+    while (x != 0){
+        int digit = x % 10;
+        reverse_num = reverse_num * 10 + digit;
+        x/=10;
+    }
+
+    cout << reverse_num << " ";
+}
+
+
+
 int main(){
-    int a = 5;
-    int b = -3;
+    // int a = 5;
+    // int b = -3;
 
-    cout << bitwise_AND(a,b) << endl;
-    cout << bitwise_OR(a,b) << endl;
-    cout << bitwise_NOT(a) << endl;
-    cout << bitwise_XOR(a,b) << endl;
+    // cout << bitwise_AND(a,b) << endl;
+    // cout << bitwise_OR(a,b) << endl;
+    // cout << bitwise_NOT(a) << endl;
+    // cout << bitwise_XOR(a,b) << endl;
 
-    cout << left_shift(5,2) << endl; // 20 --> a * 2^b
-    cout << right_shift(10,2) << endl; // 2 --> a / 2^b
+    // cout << left_shift(5,2) << endl; // 20 --> a * 2^b
+    // cout << right_shift(10,2) << endl; // 2 --> a / 2^b
 
 
-    fibonacci_series(10);
-    cout << endl;
-    prime_or_not(21);
-    cout << endl;
+    // fibonacci_series(10);
+    // cout << endl;
+    // prime_or_not(21);
+    // cout << endl;
+
+
+    // decimal_to_binary(128);
+    reverse_num(-121);
     
     return 0;
 }
